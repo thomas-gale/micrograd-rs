042822ed8076e9b8e0f859445f60515ae7f2e9c9
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-01 12:14:53 +0000

    Further tidying of the RefCell implementation.

--- src/engine.rs
+++ src/engine.rs
@@ -23,7 +23,7 @@ pub struct Value<T> {
     val: Rc<RefCell<ValueData<T>>>,
 }
 
-impl<T: fmt::Display + fmt::Debug> fmt::Display for Value<T> {
+impl<T: fmt::Display + fmt::Debug + Clone> fmt::Display for Value<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
@@ -31,7 +31,13 @@ impl<T: fmt::Display + fmt::Debug> fmt::Display for Value<T> {
             self.val.as_ref().borrow().label,
             self.val.as_ref().borrow().data,
             self.val.as_ref().borrow().op,
-            self.val.as_ref().borrow().prev
+            self.val
+                .as_ref()
+                .borrow()
+                .prev
+                .iter()
+                .map(|v| v.label())
+                .collect::<Vec<_>>()
         )
     }
 }
@@ -72,37 +78,27 @@ impl<T: Clone> Value<T> {
     }
 }
 
-// Lifetimes for the Value reference inside the BTreeSet are already getting a little gnarley - I will probably want to instead use Rc shared pointers?
-// impl<'a, T: ops::Add<Output = T> + cmp::Ord + Copy> ops::Add for &'a Value<T> {
-impl<T: ops::Add<Output = T> + cmp::Ord + Copy> ops::Add for Value<T> {
+impl<'a, T: ops::Add<Output = T> + cmp::Ord + Copy> ops::Add for &'a Value<T> {
     type Output = Value<T>;
     fn add(self, rhs: Self) -> Self::Output {
+        let op = Op::Add;
         Value::new_from_op(
-            self.val.as_ref().borrow().data + rhs.val.as_ref().borrow().data,
-            format!(
-                "({} {:?} {})",
-                self.val.as_ref().borrow().label,
-                Op::Add,
-                rhs.val.as_ref().borrow().label
-            ),
-            Op::Add,
+            self.data() + rhs.data(),
+            format!("({} {:?} {})", self.label(), op, rhs.label(),),
+            op,
             BTreeSet::from([self.clone(), rhs.clone()]),
         )
     }
 }
 
-impl<T: ops::Mul<Output = T> + cmp::Ord + Copy> ops::Mul for Value<T> {
+impl<'a, T: ops::Mul<Output = T> + cmp::Ord + Copy + Clone> ops::Mul for &'a Value<T> {
     type Output = Value<T>;
     fn mul(self, rhs: Self) -> Self::Output {
+        let op = Op::Mul;
         Value::new_from_op(
-            self.val.as_ref().borrow().data * rhs.val.as_ref().borrow().data,
-            format!(
-                "({} {:?} {})",
-                self.val.as_ref().borrow().label,
-                Op::Mul,
-                rhs.val.as_ref().borrow().label
-            ),
-            Op::Mul,
+            self.data() * rhs.data(),
+            format!("({} {:?} {})", self.label(), op, rhs.label(),),
+            op,
             BTreeSet::from([self.clone(), rhs.clone()]),
         )
     }
@@ -115,25 +111,25 @@ mod tests {
     #[test]
     fn can_create_new() {
         let v1 = Value::new(1i32, String::from("v1"));
-        assert_eq!(v1.val.as_ref().borrow().data, 1i32);
+        assert_eq!(v1.data(), 1i32);
     }
 
     #[test]
     fn can_add_value() {
         let v1 = Value::new(1i32, String::from("v1"));
         let v2 = Value::new(2i32, String::from("v2"));
-        let v3 = v1.clone() + v2;
+        let v3 = &v1 + &v2;
         println!("{}", v3);
-        println!("{}", v1); // TODO - Impossible without clone on each op
-        assert_eq!(v3.val.as_ref().borrow().data, 3i32);
+        println!("{}", v1);
+        assert_eq!(v3.data(), 3i32);
     }
 
     #[test]
     fn can_mul_value() {
         let v1 = Value::new(2i32, String::from("v1"));
         let v2 = Value::new(3i32, String::from("v2"));
-        let v3 = v1 * v2;
-        assert_eq!(v3.val.as_ref().borrow().data, 6i32);
+        let v3 = &v1 * &v2;
+        assert_eq!(v3.data(), 6i32);
     }
 
     #[test]
@@ -141,14 +137,15 @@ mod tests {
         let a = Value::new(2, String::from("a"));
         let b = Value::new(-3, String::from("b"));
         let c = Value::new(10, String::from("c"));
-        let mut e = a * b;
+        let mut e = &a * &b;
         e.set_label(String::from("e"));
-        let mut d = c + e;
+        let mut d = &c + &e;
         d.set_label(String::from("d"));
         let f = Value::new(-2, String::from("f"));
-        let mut L = f * d;
-        L.set_label(String::from("d"));
-        println!("{}", L);
-        // println!("{}", e);
+        let mut l = &f * &d;
+        l.set_label(String::from("d"));
+        println!("{}", l);
+        println!("{}", e);
+        println!("{}", b);
     }
 }
