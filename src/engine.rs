407204550f61b3b82d01b32912de764c51d1de64
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-01 00:25:23 +0000

    WIP: Starting on the prev Set of Values. TODO - decide how to manage lifetimes/RC boxes for the values in these sets.

--- src/engine.rs
+++ src/engine.rs
@@ -1,27 +1,46 @@
-use std::fmt;
-use std::ops;
+use std::collections::BTreeSet;
+use std::{cmp, fmt, ops};
 
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Value<T> {
     pub data: T,
+    prev: BTreeSet<Value<T>>,
 }
 
-impl<T: fmt::Display> fmt::Display for Value<T> {
+impl<T: fmt::Display + fmt::Debug> fmt::Display for Value<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Value (data: {}, ...)", self.data)
+        write!(f, "Value (data: {}, prev: {:?})", self.data, self.prev)
     }
 }
 
-impl<T: fmt::Display + ops::Add> Value<T> {
+impl<T> Value<T> {
     pub fn new(data: T) -> Self {
-        Self { data }
+        Self {
+            data,
+            prev: BTreeSet::new(),
+        }
+    }
+    pub fn new_with_children(data: T, children: BTreeSet<Self>) -> Self {
+        Self {
+            data,
+            prev: children,
+        }
     }
 }
 
-impl<T: fmt::Display + ops::Add<Output = T>> ops::Add for Value<T> {
+// Lifetimes for the Value reference inside the BTreeSet are already getting a little gnarley - I will probably want to instead use Rc shared pointers?
+// impl<'a, T: ops::Add<Output = T> + cmp::Ord + Copy> ops::Add for &'a Value<T> {
+impl<T: ops::Add<Output = T> + cmp::Ord + Copy> ops::Add for Value<T> {
     type Output = Value<T>;
     fn add(self, rhs: Self) -> Self::Output {
-        println!("Adding {} to {}", self, rhs);
-        Value::new(self.data + rhs.data)
+        Value::new_with_children(self.data + rhs.data, BTreeSet::from([self, rhs]))
+    }
+}
+
+impl<T: ops::Mul<Output = T>> ops::Mul for Value<T> {
+    type Output = Value<T>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Value::new(self.data * rhs.data)
     }
 }
 
@@ -30,16 +49,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_display_value() {
+    fn can_create_new() {
         let v1 = Value::new(1i32);
-        println!("{}", v1);
+        assert_eq!(v1.data, 1i32);
     }
 
     #[test]
     fn can_add_value() {
         let v1 = Value::new(1i32);
         let v2 = Value::new(2i32);
-        let v3 = v1 + v2;
+        let v3 = v1.clone() + v2;
         println!("{}", v3);
+        println!("{}", v1); // TODO - Impossible without clone on each op
+        assert_eq!(v3.data, 3i32);
+    }
+
+    #[test]
+    fn can_mul_value() {
+        let v1 = Value::new(2i32);
+        let v2 = Value::new(3i32);
+        let v3 = v1 * v2;
+        assert_eq!(v3.data, 6i32);
     }
 }
